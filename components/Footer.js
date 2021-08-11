bdb47561a42e3aa687e723dcf521f82b60fe6c4e
Omar Ouhra <omarouhra@Omars-Mac-mini.local>
2021-08-11 17:48:31 +0100

    footer component

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+function Footer() {
+  return (
+    <div className=" bg-gray-100">
+      <div className='grid grid-cols-1 md:grid-cols-4  gap-y-10 px-10 py-14 max-w-7xl  m-auto'>
+        <div className='space-y-5 text-xs text-gray-800'>
+          <h5 className='font-bold'>ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+        <div className='space-y-5 text-xs text-gray-800'>
+          <h5 className='font-bold'>COMMUNITY</h5>
+          <p>Accessibility</p>
+          <p>This is not a real site</p>
+          <p>Its a pretty awesome clone</p>
+          <p>Referrals accepted</p>
+          <p>Papafam</p>
+        </div>
+        <div className='space-y-5 text-xs text-gray-800'>
+          <h5 className='font-bold'>ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+        <div className='space-y-5 text-xs text-gray-800'>
+          <h5 className='font-bold'>COMMUNITY</h5>
+          <p>Accessibility</p>
+          <p>This is not a real site</p>
+          <p>Its a pretty awesome clone</p>
+          <p>Referrals accepted</p>
+          <p>Papafam</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
